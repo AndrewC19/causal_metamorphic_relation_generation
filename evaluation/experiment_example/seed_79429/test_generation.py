d7dcbd0daec940b2452825041c12c9dfab686b2a
Michael <jmafoster1@gmail.com>
2022-08-23 15:12:42 +0100

    Testgen works

--- evaluation/experiment_example/seed_79429/test_generation.py
+++ evaluation/experiment_example/seed_79429/test_generation.py
@@ -10,33 +10,51 @@ from causal_testing.specification.scenario import Scenario
 from causal_testing.specification.variable import Input, Output
 from causal_testing.specification.causal_dag import CausalDAG
 
+pd.set_option('display.max_columns', 500)
+
+def count(lst):
+    counts = {}
+    for item in lst:
+        if item not in counts:
+            counts[item] = 0
+        counts[item] += 1
+    return counts
 
 def metamorphic_tests(
     independence: ConditionalIndependence, scenario: Scenario, confidence: float = 0.95, sample_size: int = 1, seed=0
 ):
+    print(independence)
     X = independence.X
-    X_prime = scenario.treatment_variables[X].name
-    inputs = list(set([v.name for v in scenario.variables.values() if isinstance(v, Input) and v.name != X] + [X]))
-    inputs_prime = list(set([v.name for v in scenario.treatment_variables.values() if isinstance(v, Input)] + [X_prime]))
-    columns = inputs + list(independence.Z) + inputs_prime
+    X_prime = scenario.treatment_variables[X].name.replace("'", "_prime")
+    inputs = list(set([v.name for v in scenario.variables.values() if isinstance(v, Input) and v.name not in (list(independence.Z) + [X])] + [X]))
+    inputs_prime = [scenario.treatment_variables[x].name.replace("'", "_prime") for x in inputs]
+    columns = inputs + [x for x in independence.Z if x != X] + inputs_prime
+    assert len(set(inputs).intersection(inputs_prime)) == 0, f"{set(inputs).intersection(inputs_prime)} should be empty"
+    assert len(inputs) == len(set(inputs)), f"Input names not unique {inputs} {count(inputs)}"
+    assert len(inputs_prime) == len(set(inputs_prime)), f"Input prime names not unique {inputs_prime} {count(inputs_prime)}"
+    assert len(columns) == len(set(columns)), f"Column names not unique {columns} {count(columns)}"
     samples = pd.DataFrame(
         lhsmdu.sample(len(columns), sample_size, randomSeed=seed).T,
         columns=columns,
     )
     print(samples)
-    for col in columns[:-len(inputs)]:
+    for col in columns[:-len(inputs_prime)]:
+        print(col)
         samples[col] = lhsmdu.inverseTransformSample(scenario.variables[col].distribution, samples[col])
     for x, x_prime in zip(inputs, inputs_prime):
         samples[x_prime] = lhsmdu.inverseTransformSample(scenario.treatment_variables[x].distribution, samples[x_prime])
-    X_values = samples[[X]+inputs]
-    X_prime_values = samples[[X_prime]+inputs_prime]
+    X_values = samples[inputs]
+    X_prime_values = samples[inputs_prime]
     Z_values = samples[independence.Z]
     print(samples)
+    print("x_values", X_values.to_dict(orient="records"))
+    print("x_prime_values", [{k.replace("_prime", ""): v for k, v in values.items()} for values in X_prime_values.to_dict(orient="records")],)
+    # assert False
     # assert len(x_values) == len(x_prime_values) == len(Z_values)
     return list(
         zip(
             X_values.to_dict(orient="records"),
-            [{k.replace("'", ""): v for k, v in values.items()} for values in X_prime_values.to_dict(orient="records")],
+            [{k.replace("_prime", ""): v for k, v in values.items()} for values in X_prime_values.to_dict(orient="records")],
             Z_values.to_dict(orient="records"),
             [independence.Y] * len(X_values),
             [independence] * len(X_values)
@@ -57,7 +75,7 @@ dag = CausalDAG("DAG.dot")
 variables = [Input(x, float, uniform(0, 10)) for x in dag.graph.nodes if x.startswith("X")]
 variables += [Output(y, float, uniform(0, 10)) for y in dag.graph.nodes if y.startswith("Y")]
 
-independences = dag.list_conditional_independence_relationships(search_heuristic="minimal")
+independences = dag.list_conditional_independence_relationships(search_heuristic="min_direct")
 independences = [i for i in independences if not i.Y.startswith("X")]
 
 scenario = Scenario(set(variables))
@@ -70,3 +88,6 @@ def test_independence(run):
     control = program(**(x_value | z_values))[y]
     treatment = program(**(x_prime_value | z_values))[y]
     assert control == treatment, f"Expected control {control} to equal treatment {treatment}"
+
+if __name__ == "__main__":
+    print("independences", independences)
