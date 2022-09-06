0bf8218283de7bc1193f9304fc540ba07edaef12
Michael <jmafoster1@gmail.com>
2022-09-06 09:06:46 +0100

    Restored evaluation

---
+++ evaluation/single_experiment/seed_17612/run.sh
@@ -0,0 +1,4 @@
+cosmic-ray init ./mutation_config.toml ./mutation_config.sqlite
+cosmic-ray --verbosity=INFO baseline ./mutation_config.toml
+cosmic-ray exec ./mutation_config.toml ./mutation_config.sqlite
+cr-html ./mutation_config.sqlite > report.html
