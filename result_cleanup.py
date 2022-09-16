522bb48277280bf2823931759bc2d4754bfd76de
Michael <jmafoster1@gmail.com>
2022-09-16 11:35:24 +0100

    Relations only

--- result_cleanup.py
+++ result_cleanup.py
@@ -5,6 +5,17 @@ import sqlite3
 import pandas as pd
 
 
+def relation_passed(outcome):
+    # Independence relations - Fails if any test failed
+    if "_||_" in outcome["relation"]:
+        return len(outcome["failures"]) == 0
+    # Dependence relations - Fails if all tests failed
+    elif "-->" in outcome["relation"]:
+        return len(outcome["failures"]) < outcome["total"]
+    else:
+        raise ValueError(f"Invalid outcome relation {outcome['relation']}. Expected '_||_' or '-->'")
+
+
 parser = argparse.ArgumentParser(
     description="Parses args"
 )
@@ -34,6 +45,9 @@ for job in os.listdir(args.results):
     result = {}
     with open(job) as f:
         result["test_outcomes"] = json.load(f)
+        for relation in result["test_outcomes"]:
+            relation["passed"] = relation_passed(relation)
+            relation.pop("failures")
         if job_id != "baseline":
             result["mutation_spec"] = mutation_specs.loc[job_id].to_dict()
             result["work_result"] = work_results.loc[job_id].to_dict()
