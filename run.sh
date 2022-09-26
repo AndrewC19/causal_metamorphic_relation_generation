973820901786ebc1bbcd76d25ed66ac3098d5013
AndrewC19 <agclark2@sheffield.ac.uk>
2022-09-26 16:40:03 +0100

    Bash script now creates directory

--- run.sh
+++ run.sh
@@ -9,9 +9,12 @@
 #source activate venv
 
 cd $1
+mkdir "t${2}"
+cp mutation_config.toml "./t${2}/mutation_config.toml"
+cd "t${2}"
 #rm *.sqlite report.html *.json
 sed -i '' -e "s/-t.*/-t $2\"/" mutation_config.toml
-cosmic-ray init ./mutation_config.toml mutation_config_t${2}.sqlite
+cosmic-ray init ./mutation_config.toml mutation_config.sqlite
 cosmic-ray --verbosity=INFO baseline ./mutation_config.toml
-cosmic-ray exec ./mutation_config.toml mutation_config_t${2}.sqlite
-python ../../../../../result_cleanup.py -r ./t${2} -db mutation_config_t${2}.sqlite -o results.json
+cosmic-ray exec ./mutation_config.toml mutation_config.sqlite
+python ../../../../../../result_cleanup.py -r . -db mutation_config.sqlite -o results.json
